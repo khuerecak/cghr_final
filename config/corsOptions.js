431276dde279f745fd17e43076081475d2883e54
khuerecak <khuerecak@gmail.com>
2023-05-07 18:23:11 -0500

    fixed CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,4 @@
-const whitelist = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
-];
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
