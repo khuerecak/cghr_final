aeb20564c2818615883f5c5e22970ec411207093
khuerecak <khuerecak@gmail.com>
2023-05-07 19:33:41 -0500

    cors

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
+const connectDB = async () =>{
+    try{
+        mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
-    } catch (err){
+    } catch(err){
         console.error(err);
     }
 }
