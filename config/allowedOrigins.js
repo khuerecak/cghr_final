75d05cb48cf7cf461187d3af2addbd41c67201fc
khuerecak <khuerecak@gmail.com>
2023-05-07 19:28:40 -0500

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://legend-tarry-basil.glitch.me',
     'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
--- config/dbConn.js
+++ config/dbConn.js
@@ -12,3 +12,18 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB;
+/*const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err){
+        console.error(err);
+    }
+}
+
+module.exports = connectDB;
+*/
