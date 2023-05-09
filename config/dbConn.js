666e36aec99395fe5b4a71c6104dcd795f03b117
khuerecak <khuerecak@gmail.com>
2023-05-08 23:12:06 -0500

    CORS issues

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-    try{
-        mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
         await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
-    } catch(err){
+    } catch (err){
         console.error(err);
     }
 }
