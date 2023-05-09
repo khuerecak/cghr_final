0c02b77da49d2065e619317a6eb2c9f084230121
khuerecak <khuerecak@gmail.com>
2023-05-09 00:28:29 -0500

    port

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
+    'http://localhost:3500',
     'http://localhost:3000',
     'https://cghrfinal.khuerecak.repl.co',
     'https://legend-tarry-basil.glitch.me',
